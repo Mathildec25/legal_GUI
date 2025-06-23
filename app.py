8ba10845b334ad1a1ed5727be3e0f2810774fd20
Mathilde <mathilde.croissant25@gmail.com>
2025-06-23 16:52:19 +0200

    setup repositories

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from dash import Dash, Input, Output, html
+from dash import Dash, html
 import dash_bootstrap_components as dbc
-
-from Homepage import get_home_layout
-from Main_window import get_main_layout
-
+from layout.homepage_layout import get_home_layout
+from layout.main_layout import get_main_layout
+from callbacks.navigation import register_navigation_callbacks
+from callbacks.main_callbacks import register_main_callbacks
 
 external_stylesheets = [dbc.themes.CERULEAN]
 app = Dash(__name__, external_stylesheets=external_stylesheets)
@@ -17,20 +17,9 @@ app.layout = dbc.Container([
     ])
 ], fluid=True)
 
-@app.callback(
-    Output("homepage", "style"),
-    Output("main-interface", "style"),
-    Input("start-button", "n_clicks"),
-    Input("back-button", "n_clicks")
-)
-def switch_home_and_main(start_clicks, back_clicks):
-    if start_clicks and (not back_clicks or start_clicks > back_clicks):
-        return {"display": "none"}, {"display": "block"}
-    elif back_clicks and (not start_clicks or back_clicks >= start_clicks):
-        return {"display": "block"}, {"display": "none"}
-#Show by default the homePage
-    return {"display": "block"}, {"display": "none"}
+# Enregistrement des callbacks
+register_navigation_callbacks(app)
+register_main_callbacks(app)
 
 if __name__ == "__main__":
     app.run(debug=True)
-
